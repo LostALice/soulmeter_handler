fc0f554c5a3c254477f1da399bebc04102131998
Alice <matthew63731417@gmail.com>
2022-12-05 20:52:57 +0800

    added repo owner option Changes to be committed: 	modified:   soulmeter_handler.py

--- soulmeter_handler.py
+++ soulmeter_handler.py
@@ -4,21 +4,29 @@ import requests, os, shutil, ctypes, re
 
 from bs4 import BeautifulSoup
 
-def check_vision() -> str:
-    soup = BeautifulSoup(requests.get("https://github.com/ga0321/SoulMeter").text, "lxml")
-    soup = soup.find(href=re.compile("/ga0321/SoulMeter/releases/tag/*"))
-    return soup["href"].replace("/ga0321/SoulMeter/releases/tag/","")
+def check_vision(repo_owner: str) -> str:
+    soup = BeautifulSoup(requests.get(f"https://github.com/{repo_owner}/SoulMeter").text, "lxml")
+    soup = soup.find(href=re.compile(f"/{repo_owner}/SoulMeter/releases/tag/*"))
+    return soup["href"].replace(f"/{repo_owner}/SoulMeter/releases/tag/","")
 
-def update(vision):
-    open(f"./{vision}.zip", "wb").write(requests.get(f"https://github.com/ga0321/SoulMeter/releases/download/{vision}/SoulMeter-v{vision}.zip").content)
+
+def update(vision: str, repo_owner: str) -> None:
+    open(f"./{vision}.zip", "wb").write(requests.get(f"https://github.com/{repo_owner}/SoulMeter/releases/download/{vision}/SoulMeter-v{vision}.zip").content)
     shutil.unpack_archive(f"./{vision}.zip", ".")
 
-if not ctypes.windll.shell32.IsUserAnAdmin():
-    raise BaseException("Run as Admin/以管理員身份運行")
 
-vision = check_vision()
+def main():
+    repo_owner = "neonr-0"
+    if not ctypes.windll.shell32.IsUserAnAdmin():
+        raise BaseException("Run as Admin/以管理員身份運行")
 
-if not f"SoulMeter {vision}.exe" in os.listdir():
-    update(vision)
+    vision = check_vision(repo_owner)
 
-os.popen(f"./SoulMeter {vision}.exe")
+    if not f"SoulMeter {vision}.exe" in os.listdir():
+        update(vision,repo_owner)
+
+    os.popen(f"./SoulMeter {vision}.exe")
+
+
+if __name__ == "__main__":
+    main()
