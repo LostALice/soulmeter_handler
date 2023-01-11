43b34ebdfe2e089c2dc7eba6013d49d43e991f20
Alice <matthew63731417@gmail.com>
2023-01-11 20:11:28 +0800

    added:     config.json     delete archive function     delete old vision function     start gamefunction  # On branch master Changes to be committed: 	modified:   README.md 	modified:   soulmeter_handler.py

--- README.md
+++ README.md
@@ -1,7 +1,40 @@
-# soulmeter_handler
+<!-- Code by AkinoAlice@Tyrant_Rex -->
 
-# put it in your DPS dir
- 
-# built command: pyinstaller -F -w --icon=1.ico .\soulmeter_handler.py
+# Soulmeter_Handler
 
-# recommend create a shortcut and set as "Run as administer"
+![專案封面圖](1.ico)
+
+## Function
+
+- [x] Automatic Update DPS
+- [x] Automatic Start up SoulWorker
+
+## Installation
+
+1. Put this application in your DPS directory
+
+1. Create a shortcut and set as "Run as administer"
+
+## Config file
+```
+{
+    "KeepHistoryVision": false,
+    "KeepArchive": false,
+    "StartSoulWorker": true,
+    "RepoOwner": "neonr-0"
+}
+```
+## Config description
+- KeepHistoryVision - keep the old vision
+- KeepArchive - keep all .zip file
+- StartSoulWorker - start both DPS and soulworker
+- RepoOwner - set the repo owner
+
+## Built command
+
+```
+pyinstaller -F -w --icon=1.ico .\soulmeter_handler.py
+```
+
+## Contact
+- [x] Discord [Don't you feel your weakness#7031]
--- soulmeter_handler.py
+++ soulmeter_handler.py
@@ -1,22 +1,53 @@
 #Code by AkinoAlice@Tyrant_Rex
 
-import requests, os, shutil, ctypes, re
+import requests, os, shutil, ctypes, re, json
 
+from webbrowser import open_new
 from bs4 import BeautifulSoup
+
+def config() -> str:
+    global setting
+    try:
+        setting = json.read("config.json")
+    except:
+        setting = {
+            "KeepHistoryVision": False,
+            "KeepArchive": False,
+            "StartSoulWorker": True,
+            "RepoOwner": "neonr-0",
+        }
+        with open("config.json", "w") as setting_file:
+            setting_file.write(json.dumps(setting,indent=4))
+
+    return setting
+
+def remove_file(vision: str) -> None:
+    for history_file in os.listdir():
+        if history_file.endswith(".exe") and not setting["KeepHistoryVision"]:
+            if vision in history_file:
+                continue
+            elif "soulmeter_handler" in history_file :
+                continue
+            os.remove(history_file)
+
+        if history_file.endswith(".zip") and not setting["KeepArchive"]:
+            if vision in history_file:
+                continue
+            os.remove(history_file)
 
 def check_vision(repo_owner: str) -> str:
     soup = BeautifulSoup(requests.get(f"https://github.com/{repo_owner}/SoulMeter").text, "lxml")
     soup = soup.find(href=re.compile(f"/{repo_owner}/SoulMeter/releases/tag/*"))
     return soup["href"].replace(f"/{repo_owner}/SoulMeter/releases/tag/","")
 
-
 def update(vision: str, repo_owner: str) -> None:
     open(f"./{vision}.zip", "wb").write(requests.get(f"https://github.com/{repo_owner}/SoulMeter/releases/download/{vision}/SoulMeter-v{vision}.zip").content)
     shutil.unpack_archive(f"./{vision}.zip", ".")
 
-
 def main():
-    repo_owner = "neonr-0"
+    setting = config()
+    repo_owner = setting["RepoOwner"]
+
     if not ctypes.windll.shell32.IsUserAnAdmin():
         raise BaseException("Run as Admin/以管理員身份運行")
 
@@ -25,8 +56,15 @@ def main():
     if not f"SoulMeter {vision}.exe" in os.listdir():
         update(vision,repo_owner)
 
+    remove_file(vision)
+
     os.popen(f"./SoulMeter {vision}.exe")
 
+    if setting["StartSoulWorker"]:
+        open_new("steam://rungameid/1377580")
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except:
+        raise BaseException("Run as Admin/以管理員身份運行")
